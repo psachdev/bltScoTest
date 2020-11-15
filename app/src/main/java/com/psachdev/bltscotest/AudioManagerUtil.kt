3e417e1bfb2876f420655d5ca3e088c2de1bcaa3
Prateek Sachdeva <sachdeva852000@gmail.com>
2020-11-15 17:18:03 +0530

    init project

---
+++ app/src/main/java/com/psachdev/bltscotest/AudioManagerUtil.kt
@@ -0,0 +1,9 @@
+package com.psachdev.bltscotest
+
+import android.content.Context
+
+class AudioManagerUtil(private val context: Context) {
+    init {
+        
+    }
+}
---
+++ app/src/main/java/com/psachdev/bltscotest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.psachdev.bltscotest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mAudioManagerUtil: AudioManagerUtil
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mAudioManagerUtil = AudioManagerUtil(this)
+    }
+}
