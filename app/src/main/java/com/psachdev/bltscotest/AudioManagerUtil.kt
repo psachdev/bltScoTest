625e574f20896025487b5f2a44f8bd04840b6e7a
Prateek Sachdeva <sachdeva852000@gmail.com>
2020-11-15 17:36:11 +0530

    Add audioManagerUtil and scoStatechangeListener

--- app/src/main/java/com/psachdev/bltscotest/AudioManagerUtil.kt
+++ app/src/main/java/com/psachdev/bltscotest/AudioManagerUtil.kt
@@ -1,9 +1,35 @@
 package com.psachdev.bltscotest
 
 import android.content.Context
+import android.content.IntentFilter
+import android.media.AudioManager
 
 class AudioManagerUtil(private val context: Context) {
-    init {
-        
+    private val mAudioManager: AudioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+    private lateinit var scoStateChangeListener: ScoStateChangeListener
+
+    fun startBltSco(){
+        mAudioManager.startBluetoothSco()
     }
+
+    fun stopBltSco(){
+        mAudioManager.stopBluetoothSco()
+    }
+
+    fun isBltScoOn(){
+        mAudioManager.isBluetoothScoOn
+    }
+
+    fun registerForBltScoStateChangeListener(scoStateChangeListener: ScoStateChangeListener){
+        this.scoStateChangeListener = scoStateChangeListener
+        val intentFilter = IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED)
+        context.registerReceiver(scoStateChangeListener, intentFilter)
+    }
+
+    fun unRegisterReceiver(){
+        if(::scoStateChangeListener.isInitialized) {
+            context.unregisterReceiver(scoStateChangeListener)
+        }
+    }
+
 }
---
+++ app/src/main/java/com/psachdev/bltscotest/ScoStateChangeListener.kt
@@ -0,0 +1,25 @@
+package com.psachdev.bltscotest
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.widget.Toast
+
+class ScoStateChangeListener : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1)
+        when {
+            AudioManager.SCO_AUDIO_STATE_CONNECTED == state -> {
+                Toast.makeText(context, context.getString(R.string.sco_connected), Toast.LENGTH_SHORT).show()
+            }
+            AudioManager.SCO_AUDIO_STATE_DISCONNECTED == state -> {
+                Toast.makeText(context, context.getString(R.string.sco_disconnected), Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                Toast.makeText(context, context.getString(R.string.sco_unknown_state), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
